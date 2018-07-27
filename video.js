01c423bdd7d44dede5ceabe33f875e98dbc43ff8
AaronVelasco93 <aaronvelasco1993@outlook.com>
2018-07-26 21:48:42 -0500

    .
    .

---
+++ video.js
@@ -0,0 +1,45 @@
+var mivideo, reproducir, barra, progreso, maximo;
+maximo=600;
+function comenzar() {
+
+  mivideo = document.getElementById("mivideo");
+  reproducir = document.getElementById("reproducir");
+  barra = document.getElementById("barra");
+  progreso = document.getElementById("progreso");
+
+  reproducir.addEventListener("click", clicando, false);
+
+  progreso.addEventListener("click", adelantando, false);
+
+
+}
+
+function clicando() {
+  if ((mivideo.paused == false) && (mivideo.ended == false)) {
+    mivideo.pause();
+    reproducir.innerHTML = "play";
+  } else {
+    mivideo.play();
+    reproducir.innerHTML = "Pause";
+
+    bucle=setInterval(estado,50);
+
+
+  }
+
+}
+
+function estado(){
+  if( mivideo.ended==false ){
+    var total=parseInt(mivideo.currentTime*maximo/mivideo.duration);
+    progreso.style.width=total+"px";
+
+
+  }
+
+}
+
+
+
+}
+window.addEventListener("load", comenzar, false);
